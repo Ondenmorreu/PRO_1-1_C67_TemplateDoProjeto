7c942454fe7e56ace42646250415c2e780f6c7c9
Newton Costa <jotaqueirozoliveira@gmail.com>
2023-09-06 20:35:39 -0300

    adicionou firebase

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBebJQuSQmeDet3u4G70V7WOm5O_wH-M-g",
+    authDomain: "projeto67-git-3b3d4.firebaseapp.com",
+    databaseURL: "https://projeto67-git-3b3d4-default-rtdb.firebaseio.com",
+    projectId: "projeto67-git-3b3d4",
+    storageBucket: "projeto67-git-3b3d4.appspot.com",
+    messagingSenderId: "493500167042",
+    appId: "1:493500167042:web:d229d7c0f1e5f2f410d59b"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
